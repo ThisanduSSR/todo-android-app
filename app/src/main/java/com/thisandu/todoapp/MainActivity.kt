84df471ca89e8ae93a4145ff76b1caacd510c711
ThisanduSSR <thisanduranadheera67@gmail.com>
2026-05-29 00:42:29 +0530

    Added custom app theme and colors

--- app/src/main/java/com/thisandu/todoapp/MainActivity.kt
+++ app/src/main/java/com/thisandu/todoapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.thisandu.todoapp.navigation.AppNavigation
+import com.thisandu.todoapp.ui.theme.TodoTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -12,7 +13,10 @@ class MainActivity : ComponentActivity() {
 
         setContent {
 
-            AppNavigation()
+            TodoTheme {
+
+                AppNavigation()
+            }
         }
     }
 }
---
+++ app/src/main/java/com/thisandu/todoapp/ui/theme/Colors.kt
@@ -0,0 +1,17 @@
+package com.thisandu.todoapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF2563EB)
+
+val LightBackground = Color(0xFFF8FAFC)
+
+val DarkBackground = Color(0xFF0F172A)
+
+val CardLight = Color(0xFFFFFFFF)
+
+val CardDark = Color(0xFF1E293B)
+
+val SuccessGreen = Color(0xFF22C55E)
+
+val ErrorRed = Color(0xFFEF4444)
